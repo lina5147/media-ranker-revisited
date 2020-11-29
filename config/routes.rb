ee4dd9a0d52da8c92ae0ef0909de2ad25888aa37
Lina Do <l.do5147@gmail.com>
2020-11-29 15:39:51 -0800

    added omniauth login route and github callback route, commented out original login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Omniauth Login Route
+  get "/auth/github", as: "github_login"
+
+  # Omniauth Github callback route
+  get "/auth/:provider/callback", to: "users#create", as: "omniauth_callback"
+
   root "works#root"
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
-  post "/logout", to: "users#logout", as: "logout"
+  # get "/login", to: "users#login_form", as: "login"
+  # post "/login", to: "users#login"
+  # post "/logout", to: "users#logout", as: "logout"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
