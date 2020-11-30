65ae8662cc81a82e2fbcc19ea810092a93aac84c
Lina Do <l.do5147@gmail.com>
2020-11-29 16:48:13 -0800

    replaced logout action with destroy action, and changed the route to delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
 
   # Omniauth Login Route
   get "/auth/github", as: "github_login"
-
   # Omniauth Github callback route
   get "/auth/:provider/callback", to: "users#create", as: "omniauth_callback"
+  # logging out
+  delete "/logout", to: "users#destroy", as: "logout"
 
   root "works#root"
-  # get "/login", to: "users#login_form", as: "login"
-  # post "/login", to: "users#login"
-  post "/logout", to: "users#logout", as: "logout"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
