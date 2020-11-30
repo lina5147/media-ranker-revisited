856dbe32391a1011fe55a49bf6355729349ba110
Lina Do <l.do5147@gmail.com>
2020-11-29 16:18:16 -0800

    add validation for uniquness of uid in the scope of provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
+  validates :uid, uniqueness: { scope: :provider}
   validates :username, uniqueness: true, presence: true
 end
