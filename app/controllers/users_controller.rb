3df77cdaba530ed9489bdc0060b7a4067009e642
Lina Do <l.do5147@gmail.com>
2020-11-29 16:31:22 -0800

    finished out the create method in User controller, create a new user and handle failure of creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,21 @@ class UsersController < ApplicationController
     if user # User exists
       flash[:status] = :success
       flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else # User doesn't exist yet (new user)
+    else # User doesn't exist yet (create new user)
+      user = User.build_from_github(auth_hash)
 
+      if user.save
+        session[:user_id] = user.id
+        flash[:status] = :success
+        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+      else
+        flash[:status] = :failure
+        flash[:result_text] = "Could not log in"
+        flash[:messages] = user.errors.messages
+        return redirect_to root_path
+      end
     end
-
+    # if we get here, we have a valid user instance
     session[:user_id] = user.id
     redirect_to root_path
   end
