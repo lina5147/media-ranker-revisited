2743b4b404d27dc8477230f7f8237ffeac777ec6
Lina Do <l.do5147@gmail.com>
2020-11-29 16:12:08 -0800

    changed data type for uid to a string

--- db/migrate/20201130000732_add_uid_and_provider_to_users.rb
+++ db/migrate/20201130000732_add_uid_and_provider_to_users.rb
@@ -1,6 +1,6 @@
 class AddUidAndProviderToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :uid, :integer
+    add_column :users, :uid, :string
     add_column :users, :provider, :string
     add_column :users, :email, :string
     add_column :users, :name, :string
