51cb66b4a45ca538d879151f3cdf3b2def00932c
Lina Do <l.do5147@gmail.com>
2020-11-29 21:35:10 -0800

    added test for redirect to work pagge if no  user is logged in

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -189,7 +189,12 @@ describe WorksController do
 
   describe "upvote" do
     it "redirects to the work page if no user is logged in" do
-      skip
+      expect {
+        post upvote_path(existing_work)
+      }.wont_change "Vote.count"
+
+      must_redirect_to work_path(existing_work)
+      expect(flash[:result_text]).must_equal "You must log in to do that"
     end
 
     it "redirects to the work page after the user has logged out" do
