37b63625c543575f46afb0ca6b9c35b3a691ce07
Lina Do <l.do5147@gmail.com>
2020-11-29 16:25:32 -0800

    added build from github method to create a user from the information in auth_hash in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
 
   validates :uid, uniqueness: { scope: :provider}
   validates :username, uniqueness: true, presence: true
+
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash["uid"]
+    user.provider = auth_hash["provider"]
+    user.username = auth_hash["nickname"]
+    user.name = auth_hash["name"]
+    user.email = auth_hash["email"]
+
+    return user
+  end
 end
